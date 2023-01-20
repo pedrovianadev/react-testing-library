945a2d1ed26afa2625e57eb5c922b50005b743b1
Pedro Viana <pedroviana.developer13@gmail.com>
2023-01-20 15:52:09 -0300

    requisito FavoritePokemon feito

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helpers/renderWithRouter';
+import { FavoritePokemon } from '../pages';
+import App from '../App';
+
+describe('Testa o componente <FavoritePokemon.js />', () => {
+  test('Testa se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemon />);
+
+    const warning = screen.getByText(/No favorite Pokémon found/i);
+    expect(warning).toBeInTheDocument();
+  });
+
+  test('Testa se são exibidos na tela apenas os Pokémon favoritados', () => {
+    renderWithRouter(<App />);
+
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetails);
+
+    const favorite = screen.getByRole('checkbox', { name: /pokémon favoritado?/i });
+    userEvent.click(favorite);
+
+    const favoritePokemons = screen.getByRole('link', { name: /favorite pokémon/i });
+    userEvent.click(favoritePokemons);
+
+    const pokemon = screen.getByText(/pikachu/i);
+    expect(pokemon).toBeInTheDocument();
+  });
+});
