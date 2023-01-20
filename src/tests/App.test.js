6878643004531d1368d21f541890a6a88113a715
Pedro Viana <pedroviana.developer13@gmail.com>
2023-01-20 14:41:13 -0300

    Começando o projeto - Requisito 1 feito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente <App.js />', () => {
+  test('É exibido na tela um link com o texto Home', () => {
+    renderWithRouter(<App />);
+
+    const home = screen.getByRole('link', { name: /home/i });
+    expect(home).toBeInTheDocument();
+  });
+
+  test('É exibido na tela um link com o texto About', () => {
+    renderWithRouter(<App />);
+
+    const about = screen.getByRole('link', { name: /about/i });
+    expect(about).toBeInTheDocument();
+  });
+
+  test('É exibido na tela um link com o texto Favorite Pokémon', () => {
+    renderWithRouter(<App />);
+
+    const favorite = screen.getByRole('link', { name: /favorite pokémon/i });
+    expect(favorite).toBeInTheDocument();
+  });
+});
